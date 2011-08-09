5b3aa6e1c1d37514a7c687e18aa3768a6dbc40f4
Elliot Yates <eyates@facebook.com>
2011-08-09 16:41:29 +0100

    Added event descriptions

--- javascript/events.js
+++ javascript/events.js
@@ -25,8 +25,9 @@ $(document).ready(function() {
 				start: '2011-10-14 19:30:00'
 			},
 			{
-				title: "Dr Nichol Health at Guantanamo Bay (Location TBC)",
-				start: '2011-11-14 18:30:00'
+				title: "Dr Nicholl Health at Guantanamo Bay (Location TBC)",
+				start: '2011-11-14 18:30:00',
+				description: "Dr David Nicholl is a neurologist, human rights activist, fundraiser for Amnesty International, and online columnist from Belfast, Northern Ireland. In March 2006 he initiated a letter in the medical journal The Lancet, signed by more than 250 medical experts urging the United States to stop force-feeding at the Guantanamo Bay and close down the prison camp. Nicholl holds the position of consultant neurologist at City Hospital Hospital & Queen Elizabeth Hospital, Birmingham, and is honorary senior lecturer at the University of Birmingham."
 			},
 			{
 				title: "World AIDS Day Krispy Kreme and cake sale",
@@ -39,14 +40,23 @@ $(document).ready(function() {
 			{
 				title: "Medsin Grants",
 				start: '2011-12-13'
-			},
+			}
 			// {
 			// 	title: 'Click for Google',
 			// 	start: new Date(y, m, 28),
 			// 	end: new Date(y, m, 29),
 			// 	url: 'http://google.com/'
 			// }
-		]
+		],
+		
+		eventRender: function(event, element) {
+			if (event.description) {
+			element.qtip({
+				content: event.description
+			});
+			}
+		}
+		
 	});
-
+	
 });
