d202c2c82792765f738f1ab289ef7b744b7a5cbb
Elliot Yates <eyates@facebook.com>
2011-08-09 16:06:22 +0100

    Added dates

--- javascript/events.js
+++ javascript/events.js
@@ -9,31 +9,43 @@ $(document).ready(function() {
 		editable: false,
 		events: [
 			{
-				title: 'All Day Event',
-				start: new Date(y, m, 1)
+				title: 'Medsoc Presentation (with Krispy Kreme sale)',
+				start: '2011-09-22'
 			},
 			{
-				title: 'Long Event',
-				start: new Date(y, m, d-5),
-				end: new Date(y, m, d-2)
+				title: "Freshers' fair",
+				start: '2011-09-23'
 			},
 			{
-				title: 'Meeting',
-				start: new Date(y, m, d, 10, 30),
-				allDay: false
+				title: "Medsin Explained (LT3)",
+				start: '2011-10-04 18:30:00'
 			},
 			{
-				title: 'Lunch',
-				start: new Date(y, m, d, 12, 0),
-				end: new Date(y, m, d, 14, 0),
-				allDay: false
+				title: "Bristol Pear Student Stop AIDS Speaker Tour",
+				start: '2011-10-14 19:30:00'
 			},
 			{
-				title: 'Click for Google',
-				start: new Date(y, m, 28),
-				end: new Date(y, m, 29),
-				url: 'http://google.com/'
-			}
+				title: "Dr Nichol Health at Guantanamo Bay (Location TBC)",
+				start: '2011-11-14 18:30:00'
+			},
+			{
+				title: "World AIDS Day Krispy Kreme and cake sale",
+				start: '2011-12-01'
+			},
+			{
+				title: "Health Poverty Action - Careers Abroad (TBC)",
+				start: '2011-12-05'
+			},
+			{
+				title: "Medsin Grants",
+				start: '2011-12-13'
+			},
+			// {
+			// 	title: 'Click for Google',
+			// 	start: new Date(y, m, 28),
+			// 	end: new Date(y, m, 29),
+			// 	url: 'http://google.com/'
+			// }
 		]
 	});
 
